17a444b1f74fa779abd37f4be28bc91ef8e81c0d
Kimbsy <lordkimber@gmail.com>
2021-04-18 10:25:05 +0100

    add todo

--- src/ssss_expressions/common.clj
+++ src/ssss_expressions/common.clj
@@ -1,6 +1,8 @@
 (ns ssss-expressions.common)
 
-(def palette ["85ffc7" "297373" "ff8552" "e6e6e6" "39393a" "BD4089" "33658A"])
+(def palette ["85ffc7" "297373" "ff8552" "e6e6e6"
+              "39393a" "bd4089" "bd80a5" "33658a"
+              "86bbd8" "9edafa"])
 
 (def light-green [133 255 199])
 (def dark-green [41 115 115])
@@ -8,4 +10,7 @@
 (def white [230 230 230])
 (def grey [57 57 58])
 (def pink [189 64 137])
-(def blue [51 101 138])
+(def highlight-pink [189 128 165])
+(def dark-blue [51 101 138])
+(def light-blue [134 187 216])
+(def highlight-blue [158 218 250])
